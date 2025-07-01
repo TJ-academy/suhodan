d10cc88de5670757f63d7e29abaaaf0b1078e7b0
seonah <seona1430@gmail.com>
2025-07-01 12:25:49 +0900

    initial commit

---
+++ src/main/java/com/example/suhodan/SuhodanApplication.java
@@ -0,0 +1,13 @@
+package com.example.suhodan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhodanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhodanApplication.class, args);
+	}
+
+}
