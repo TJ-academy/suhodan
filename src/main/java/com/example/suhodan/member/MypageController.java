fbf9d25ff4205297a5c8888429d484779002582e
seonah <seona1430@gmail.com>
2025-07-10 13:13:40 +0900

    아이콘 추가 및 마이페이지 메인

--- src/main/java/com/example/suhodan/member/MypageController.java
+++ src/main/java/com/example/suhodan/member/MypageController.java
@@ -2,17 +2,24 @@ package com.example.suhodan.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.suhodan.member.MemberDAO;
+import com.example.suhodan.member.MemberDTO;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MypageController {
-	@Autowired 
-	MemberDAO memberDao;
-	
-	//나의 수호수
-	@GetMapping("/mypage/mytree")
-	public String mytree() {
-		return "member/mypage/mytree";
-	}
+
+    @Autowired 
+    MemberDAO memberDao;
+
+    // 나의 수호수
+    @GetMapping("/mypage/mytree")
+    public String mytree() {
+        return "member/mypage/mytree";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/views/member/mypage/mypage_addressedit.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head><title>비밀번호 변경</title></head>
+<body>
+<h2>비밀번호 변경 페이지 (작업 중)</h2>
+</body>
+</html>
