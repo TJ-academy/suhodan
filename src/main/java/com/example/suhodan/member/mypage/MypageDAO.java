6fafd631e31da7cb8a9bdb2c5844af70afa7bc82
ssong <9yeosong@naver.com>
2025-07-16 13:00:57 +0900

    유저배지 테이블에 id 추가. 명패함에서 중복배지는 자세히보기 안됐던거 수정.

--- src/main/java/com/example/suhodan/member/mypage/MypageDAO.java
+++ src/main/java/com/example/suhodan/member/mypage/MypageDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface MypageDAO {
 	List<MypageDTO> getUserBadge(String user_id);
-	MypageDTO badgeDetail(String user_id, int badge_id);
+	MypageDTO badgeDetail(int userbadge_id);
 	// 배지 목록 페이징용
 	List<MypageDTO> getUserBadgesPaged(String user_id, int startRow, int endRow);
 
